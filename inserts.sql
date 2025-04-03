478ab73ba4202211faf7988284b22164420f1d51
SaidRem <said.r@yandex.ru>
2025-04-03 09:43:01 +0400

    Added words inserts

---
+++ inserts.sql
@@ -0,0 +1,17 @@
+INSERT INTO words (english, russian) VALUES
+    ('apple', 'яблоко'),
+    ('book', 'книга'),
+    ('car', 'машина'),
+    ('dog', 'собака'),
+    ('elephant', 'слон'),
+    ('flower', 'цветок'),
+    ('guitar', 'гитара'),
+    ('house', 'дом'),
+    ('ice', 'лёд'),
+    ('juice', 'сок'),
+    ('key', 'ключ'),
+    ('lamp', 'лампа'),
+    ('moon', 'луна'),
+    ('notebook', 'блокнот'),
+    ('orange', 'апельсин');
+    
