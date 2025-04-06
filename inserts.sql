1e00bf02660e8b4c3da82e7e6f7fe200edbdb94b
SaidRem <said.r@yandex.ru>
2025-04-06 23:57:11 +0400

    Fixed tables in sql queries

--- inserts.sql
+++ inserts.sql
@@ -1,17 +1,17 @@
-INSERT INTO words (english, russian) VALUES
-    ('apple', 'яблоко'),
-    ('book', 'книга'),
-    ('car', 'машина'),
-    ('dog', 'собака'),
-    ('elephant', 'слон'),
-    ('flower', 'цветок'),
-    ('guitar', 'гитара'),
-    ('house', 'дом'),
-    ('ice', 'лёд'),
-    ('juice', 'сок'),
-    ('key', 'ключ'),
-    ('lamp', 'лампа'),
-    ('moon', 'луна'),
-    ('notebook', 'блокнот'),
-    ('orange', 'апельсин');
+INSERT INTO words (english, russian, added_by) VALUES
+    ('apple', 'яблоко', NULL),
+    ('book', 'книга', NULL),
+    ('car', 'машина', NULL),
+    ('dog', 'собака', NULL),
+    ('elephant', 'слон', NULL),
+    ('flower', 'цветок', NULL),
+    ('guitar', 'гитара', NULL),
+    ('house', 'дом', NULL),
+    ('ice', 'лёд', NULL),
+    ('juice', 'сок', NULL),
+    ('key', 'ключ', NULL),
+    ('lamp', 'лампа', NULL),
+    ('moon', 'луна', NULL),
+    ('notebook', 'блокнот', NULL),
+    ('orange', 'апельсин', NULL);
     
